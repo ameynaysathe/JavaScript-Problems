84738e7b52f2b07b21e6ea445e861a991fb189d1
Amey Naysathe <amey@Ameys-MacBook-Air.local>
2025-11-18 11:50:59 +0530

    adding 3 javascript files

---
+++ 02.js
@@ -0,0 +1,7 @@
+// Check if a number is even or odd.
+
+function isEvenOrOdd(num){
+    return num % 2 === 0 ? "Even" : "Odd";
+}
+console.log(isEvenOrOdd(4)); // "Even"
+console.log(isEvenOrOdd(7)); // "Odd"
---
+++ 03.js
@@ -0,0 +1,7 @@
+// Find the largest of three numbers.
+
+function largestOfThree(a, b, c) {
+    return Math.max(a, b, c);
+}
+console.log(largestOfThree(10, 5, 8)); // 10
+console.log(largestOfThree(3, 12, 7)); // 12
